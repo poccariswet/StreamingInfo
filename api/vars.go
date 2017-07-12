60a3ed4287cc0d2ff0052fad8c93b967a43100b3
soeyusuke <s1240231@gmail.com>
2017-07-12 23:06:29 +0900

    [add] データ取得...

---
+++ api/vars.go
@@ -0,0 +1,37 @@
+package main
+import ()
+
+var (
+  varsLock sync.RWMutex
+  vars map[*http.Request]map[string]interface{}
+)
+
+// リクエストのためのデータ保持用
+func OpenVars(r *http.Reqest) {
+  varsLock.Lock()
+  if vars == nil {
+    vars = map[*http.Request]map[string]interface{}{}
+  }
+  vars[r] = map[string]interface{}
+  varsLock.Unlock()
+}
+
+func CloseVars(r *http.Request) {
+  varsLock.Lock()
+  delete(vars, r)
+  varsLock.Unlock()
+}
+
+// データ読み出し
+func GetVar(r *http.Request, key string) interface{} {
+  varsLock.RLock()
+  value := vars[r][key]
+  varsLock.RUnlock()
+  return value
+}
+
+func SetVar(r *http.Request, key string, value interface{}) {
+  varsLock.Lock()
+  vars[r][key] = value
+  varsLock.Unlock()
+}
