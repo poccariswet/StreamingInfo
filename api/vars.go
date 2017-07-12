25d62675c599ea05324af716041dab8d76ecb13c
soeyusuke <s1240231@gmail.com>
2017-07-12 23:45:08 +0900

    [add] データ取得...

--- api/vars.go
+++ api/vars.go
@@ -1,37 +1,38 @@
 package main
+
 import ()
 
 var (
-  varsLock sync.RWMutex
-  vars map[*http.Request]map[string]interface{}
+	varsLock sync.RWMutex
+	vars     map[*http.Request]map[string]interface{}
 )
 
 // リクエストのためのデータ保持用
-func OpenVars(r *http.Reqest) {
-  varsLock.Lock()
-  if vars == nil {
-    vars = map[*http.Request]map[string]interface{}{}
-  }
-  vars[r] = map[string]interface{}
-  varsLock.Unlock()
+func OpenVars(r *http.Request) {
+	varsLock.Lock()
+	if vars == nil {
+		vars = map[*http.Request]map[string]interface{}{}
+	}
+	vars[r] = map[string]interface{}{}
+	varsLock.Unlock()
 }
 
 func CloseVars(r *http.Request) {
-  varsLock.Lock()
-  delete(vars, r)
-  varsLock.Unlock()
+	varsLock.Lock()
+	delete(vars, r)
+	varsLock.Unlock()
 }
 
 // データ読み出し
 func GetVar(r *http.Request, key string) interface{} {
-  varsLock.RLock()
-  value := vars[r][key]
-  varsLock.RUnlock()
-  return value
+	varsLock.RLock()
+	value := vars[r][key]
+	varsLock.RUnlock()
+	return value
 }
 
 func SetVar(r *http.Request, key string, value interface{}) {
-  varsLock.Lock()
-  vars[r][key] = value
-  varsLock.Unlock()
+	varsLock.Lock()
+	vars[r][key] = value
+	varsLock.Unlock()
 }
