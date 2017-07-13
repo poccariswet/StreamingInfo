917f25ab046b30000ec443ae3dc758f3759f6bce
soeyusuke <s1240231@gmail.com>
2017-07-14 01:02:19 +0900

    [add] fixed

--- api/polls.go
+++ api/polls.go
@@ -27,7 +27,7 @@ func handlePolls(w http.ResponseWriter, r *http.Request) {
 		handlePollsDelete(w, r)
 		return
 	case "OPTIONS":
-		w.Header().Add("Access-Control-Allow-Method", "DELETE")
+		w.Header().Add("Access-Control-Allow-Methods", "DELETE")
 		respond(w, r, http.StatusOK, nil)
 		return
 	}
